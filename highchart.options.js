76e89e81c7f99b308f79769a33914e0e83eaa701
pixelsyndicate <wdobson@pixelsyndicate.com>
2015-05-20 17:19:54 -0500

    compacted code to run as asp.net web page project

---
+++ highchart.options.js
@@ -0,0 +1,53 @@
+﻿var numberFormatter = new utilities.numbers.stringNumber();
+var highCharts = Highcharts;
+// configure the highchart using Highcharts.setOptions()
+$(function () {
+
+
+
+
+
+
+    // these are ways to override the base chart
+    highCharts.setOptions({
+        chart: {
+            style: {
+                fontFamily: 'tahoma'
+            },
+            backgroundColor: 'transparent'
+        },
+        tooltip: {
+            backgroundColor: null,
+            borderWidth: 0,
+            shadow: false,
+            shared: true,
+            useHTML: true,
+            style: {
+                padding: 0
+            }
+        }
+    });
+
+    // these are ways to override the base chart
+    highCharts.setOptions({
+        tooltip: {
+            formatter: function () {
+
+                // formatter returns 'headerFormat', 'pointFormat' and 'footerFormat'
+                var s = '<div class="tooltip-header">' + this.x.replace('<br>', ' - ') + '</div><table class="tooltip-table">';
+                $.each(this.points, function (i, point) {
+                    s += '<tr data-ident="graphs.funnel_dual_fund.js"><td class="tooltip-label">' + this.series.name + '</td><td class="tooltip-value">';
+                    if (point.y >= 1000000) {
+                        s += numberFormatter.roundMillions(point.y, 1);
+                    } else {
+                        s += numberFormatter.roundThousands(point.y, 2);
+                    }
+                    s += '</td></tr>';
+                });
+                s += '</table>';
+                return s;
+            }
+        }
+    });
+
+});
--- utilities.numbers.js
+++ utilities.numbers.js
@@ -61,6 +61,41 @@ utilities.numbers = function () {
     return that;
 };
 
+utilities.numbers.stringNumber = function () {
+    var self = this;
+    self.valueToFormat = 100;
+
+    // returns double
+    self.decimals = function (c) {
+        return (this.valueToFormat).toFixed(c);
+    };
+
+    // return string
+    self.currency = function (value, count) {
+
+        self.valueToFormat = value;
+        return '$' + self.decimals(count).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + 'M';
+    };
+
+    // returns string
+    self.percent = function (v) {
+        self.valueToFormat = (v <= 1) ? (v * 100) : v;
+        return self.valueToFormat.toString() + '%';
+    };
+
+    // return string
+    self.roundMillions = function (value, count) {
+        this.valueToFormat = (value / 1000000);
+        return self.decimals(count).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + 'M';
+    };
+
+    // returns string
+    self.roundThousands = function (value, count) {
+        this.valueToFormat = (value / 1000);
+        return self.decimals(count).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + 'K';
+    };
+};
+
 utilities.randomIntFromInterval = function (min, max) {
 
 
